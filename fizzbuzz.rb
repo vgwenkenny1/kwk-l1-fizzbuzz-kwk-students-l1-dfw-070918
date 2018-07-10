c66b2dd3dc2ae8bbdeedc8baf640fedde21e8e71
vgwenkenny1 <vgwenkenny1@gmail.com>
2018-07-10 21:37:45 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(int)
+ elsif int % 3 == 0 && % 5 == 0
+    return "FizzBuzz"
+  if int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0 
+    return "Buzz"
+  end
+end
+
+fizzbuzz(15)
